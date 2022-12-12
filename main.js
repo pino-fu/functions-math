0843f7e6fea79a22889cc800bc9e370e7f82bf50
Ben Hoberg <benjhoberg@gmail.com>
2022-12-12 10:26:40 -0600

    JS challenge involving simple math.

---
+++ main.js
@@ -0,0 +1,15 @@
+const adder = (num1, num2) => {
+    const sum = num1 + num2
+    return sum
+}
+
+const doubler = (num) => {
+    result = num * 2
+    return result
+}
+
+const addTwoSeven = adder(2,7)
+
+const doubleSumTwoSeven = doubler(addTwoSeven);
+
+console.log(doubleSumTwoSeven);
